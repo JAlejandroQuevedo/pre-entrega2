7de6d9e8af67374c0afbb7e8a2a85cb9d9e88657
JAlejandroQuevedo <alejandro_ya23@hotmail.com>
2023-12-21 07:41:42 -0700

    Tercer commit de la entrega

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
+
 function ingresarGlucosa() {
     //Definicion de la funcion
     let registrosGlucosa = [];
     //Se crea un array paa almacenar los registros
     const MAXREGISTROS = 5;
     //Se establece un numero maximo de registros que se pueden ingresar
+    prompt("Ingresa una cifra de glucosa, si esta se encuentra fuera del rango de 70 y 100, se te notificara")
 
     for (let i = 1; i <= MAXREGISTROS; i++) {
         //Se crea un bucle para ingresar los datos
         let glucosa = parseInt(prompt("Ingrese tu cifra de glucosa (registro " + i + "):"));
         //Soliciar al usuario ingresar los datos
-        if (glucosa > 70 || glucosa > 100) {
+        if (glucosa >= 110) {
             alert("Tu glucosa se encuentra elevada, favor de ir al medico")
             //Se notifica al usuario si su nivel de glucosa se encuentra elevado.
+        } else if(glucosa <= 70){
+            alert("Tu glocosa se encuentra demasiado baja, por favor, consume algo dulce o acude al doctor");
+            //Se notificara al usuario si se encuentra demasiado baja
         } else if (isNaN(glucosa)) {
             alert("Por favor ingresa numeros validos");
             i--;
@@ -26,12 +31,22 @@ function ingresarGlucosa() {
     console.log("Registros de glucosa: ");
     for (let i = 0; i < registrosGlucosa.length; i++) {
         let registro = registrosGlucosa[i];
-        console.log(
-            "Tu registro " + (i + 1) + ": de glucosa es = " + registro.glucosa + ", lo normal es entre 70 y 100 mg/dl"
-        );
+        if(registro.glucosa >= 110){
+            console.log(
+                "Tu registro " + (i + 1) + ": de glucosa es = " + registro.glucosa + ", se encuentra elevado, favor de ir al medico."
+            );
+        }else if(registro.glucosa <= 70){
+            console.log(
+                "Tu registro " + (i + 1) + ": de glucosa es = " + registro.glucosa + ", se encuentra bajo, favor de ir al medico o consumir una fruta."
+            );
+        }else{
+            console.log(
+                "Tu registro " + (i + 1) + ": de glucosa es = " + registro.glucosa + ", es normal"
+            );
+        }
     }
     /*Mostrar los registros al usuario, se usa el bucle for para recorrer el array u mostrar cada registro,
-    de la glucosa tomada por el prompt.
+    de la glucosa tomada por el prompt y notifica en consola si su glocosa es normal o si esta se encuentra elevada.
     */
 
 }
